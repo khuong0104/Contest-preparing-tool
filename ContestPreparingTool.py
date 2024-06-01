c91798a9f22002530a16ff62aaeb027faf0da530
Hữu Khương <84231576+khuong0104@users.noreply.github.com>
2024-06-01 18:29:38 +0700

    Update ContestPreparingTool.py
    Snippet

--- ContestPreparingTool.py
+++ ContestPreparingTool.py
@@ -1,13 +1,24 @@
 import os 
 
-def createFile(n,nowPath):
+def createFile(n,nowPath,snippetPath):
 	problems = chr(n + 65)
 	CrPath = os.path.join(nowPath,problems)
 	os.mkdir(CrPath) 
 	print("Directory ",CrPath," created" )
 	os.chdir(CrPath)
 	f = open(problems+".cpp", "a")
-	f.write("#include <bits/stdc++.h> \nusing namespace std; \nint main(){\n }")
+	x = open(problems+".in","a")
+	x.close()
+	x = open(problems+".out","a")
+	x.close()
+	x = open("DEBUG.txt","a")
+	x.close()
+	os.chdir(snippetPath)
+	t = open("snippet.txt","r")
+	f.write('string TASK = "'+problems+'";\n')
+	for line in t:
+		f.write(line)
+	t.close()
 	f.close()
 def createNameOfContestFile(nowPath):
 	Name = input("Contest name: ")
@@ -20,6 +31,7 @@ print("Welcome to Competitive programming creation tool")
 print("Press Ctrl + C to close this tool")
 print("Your directory: ",os.getcwd())
 c = 0 
+snippetPath = os.getcwd()
 while True :
 	if (c>0):
 		cmd = input("Want to exit?(Y/N) ")
@@ -41,5 +53,5 @@ while True :
 	nowPath = createNameOfContestFile(nowPath)
 	AmountOfProblem = int(input("Amount Of Problems :"))
 	for i in range(AmountOfProblem):
-		createFile(i,nowPath)
+		createFile(i,nowPath,snippetPath)
 	c+=1
